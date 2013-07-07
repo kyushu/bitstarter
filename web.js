4e15e6b38495be022e3bc71e85a015d978ec73ce
kyushu <lens828@gmail.com>
2013-07-07 15:40:21 +0000

    read from index.html

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+var index = new buffer(fs.readFileSync('index.html'), "utf-8");
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+//  response.send('Hello World!');
+  response.send(index.toString());
 });
 
 var port = process.env.PORT || 5000;
